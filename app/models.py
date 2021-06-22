1b92f31e663bce90f5e307e98688f82fd90c70bf
Tinashe Gondwa <tinashemgondwa@gmail.com>
2021-06-22 12:39:10 +0200

    Working on Users and Verification Views

--- app/models.py
+++ app/models.py
@@ -25,36 +25,3 @@ class File(models.Model):
 
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager
-
-    # def get_absolute_url(self):
-    #     return reverse('filemanager:file_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
-
-# class User(AbstractUser):
-    # is_organiser = models.BooleanField(default=True)
-    # is_agent = models.BooleanField(default=False)
-
-# class Department(models.Model):
-#     pass
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
-#     first_name = models.CharField(max_length=200, null=True, blank=True)
-#     last_name = models.CharField(max_length=200, null=True, blank=True)
-#     phone = models.CharField(max_length=200, null=True, blank=True)
-
-#     def __str__(self):
-#         return str(self.first_name)
-
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#         print('Profile created!')
-
-# post_save.connect(create_profile, sender=User)
-
-# def update_profile(sender, instance, created, **kwargs):
-#     if created == False:
-#         instance.Profile.save()
-#         print('Profile updated!')
-
-# post_save.connect(update_profile, sender=User)
